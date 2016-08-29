05a343103e7e8b4d7578064c87ea51488693eee5
Lebedenko Tatiana <lebedenko_tatiana@mail.ru>
2016-08-29 16:26:51 +0300

    first commit

---
+++ js/jquery.carousel.js
@@ -0,0 +1,53 @@
+﻿
+(function($) {
+  
+  $.fn.carousel = function() {
+
+    var $leftArrow = $('.carousel__prev');
+    var $rightArrow = $('.carousel__next');
+
+    var $elementsList = $('.carousel');
+ 
+    var shiftPx = $elementsList.find('.carousel__item').outerWidth(true);
+    var currentLeftValue = 0;
+  
+    var elementsCount = $elementsList.find('.carousel__item').length;
+
+    var wrapperWidth = $elementsList.parent('.carousel-wrapper').width();
+    var imgWidth = $elementsList.find('.carousel__item img').width()
+    var elementsWrapper = Math.floor(wrapperWidth/imgWidth);
+
+    var minOffset = - ((elementsCount - elementsWrapper) * shiftPx);
+    var maxOffset = 0;
+
+    $leftArrow.click(function() {
+      if (currentLeftValue != maxOffset) {
+        currentLeftValue += shiftPx;
+        $elementsList.animate({left: currentLeftValue + 'px'}, {queue:false, duration:600});
+        $rightArrow.removeClass('inactive');
+      }        
+      if (currentLeftValue == maxOffset) {
+        $leftArrow.addClass('inactive');
+      }
+
+    });
+ 
+    $rightArrow.click(function() {
+      if (currentLeftValue != minOffset) {
+        currentLeftValue -= shiftPx;
+        $elementsList.animate({left: currentLeftValue + 'px'}, {queue:false, duration:600});
+        $leftArrow.removeClass('inactive');
+      }        
+      if (currentLeftValue == minOffset) {
+        $rightArrow.addClass('inactive');
+      }
+
+    });
+
+    return this;
+
+  }
+
+
+})(jQuery);
+
---
+++ js/sqript.js
@@ -0,0 +1,37 @@
+﻿
+$(function() {
+
+  $('.carousel__prev').carousel(); 
+  $('.carousel__next').carousel(); 
+
+});
+
+
+
+$(function () {
+  var html = $('#hw').html();
+  var data = 
+    {
+     fio: 'Лебеденко Татьяна Валериевна',
+     imgCaption: 'Студентка курса GoFrontend Online#2',
+     listHead: 'Хочу учить фронтенд, потому что:',
+     list: [
+	      'учиться и постоянно приобретать новые знания - это здорово', 
+	      'фронтенд - это перспективное web-направление', 
+	      'обучение по данному курсу расширит сферу моих профессиональных знаний и навыков'
+	   ],
+     phone: 'Мой контактный телефон',
+     phoneNum: '+380509158346',
+     accountFB: 'Мой аккаунт в ФБ',
+     accountHref: 'http://facebook.com',
+     feedBack: 'Мой фидбек',
+     feedList: [
+		'Это первая попытка написания html-кода',
+		'А вот забор построить я не смогу... '
+		]
+    }
+
+  var content = tmpl(html, data);
+  $('body').append(content);
+
+});
